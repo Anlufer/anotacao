be535e4cb3e6cdfa3f51fd8f8303007d755ffeca
Bruno Rafael Sant' Ana <bruno.fullstackdeveloper@gmail.com>
2019-11-01 22:16:45 -0300

    Implementadas as features para adicionar anotacao e gravar anotacoes

--- src/br/com/anderson/json/AnotacaoHandler.java
+++ src/br/com/anderson/json/AnotacaoHandler.java
@@ -3,6 +3,8 @@ package br.com.anderson.json;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
@@ -14,14 +16,51 @@ import br.com.anderson.model.Anotacao;
 public class AnotacaoHandler {
 	
 	private Gson gson;
+	private String caminhoArquivoJson = "/home/bruno/eclipse-2019‑06-workspace/anotacoes/dados/anotacoes.json";
 	
 	public AnotacaoHandler() {
 		gson = new Gson();
 	}
 	
+	public void adicionarAnotacao(Anotacao anotacao) throws IOException {
+		
+		ArrayList<Anotacao> anotacoes = getTodasAnotacoes();
+		boolean achou = false;
+		
+		for(Anotacao a : anotacoes) {
+			if(a.getTitulo().equals(anotacao.getTitulo())) {
+				achou = true;
+				break;
+			}
+		}
+		
+		if(achou == true) {
+			System.out.println("Titulo ja existente");
+		}else {			
+			anotacoes.add(anotacao);
+			gravarAnotacoes(anotacoes);
+			System.out.println("Anotacao adicionada com sucesso");
+		}
+		
+	}
+	
+	public void removerAnotacao(String titulo) {
+		//1-iterar usando for na lista de todas anotações(usa o método getTodasAnotacoes para pegar a lista)
+		//2-verificar se algum item da lista tem esse titulo passado como argumento e remover ele da lista
+		//3-se o título não existir exibe mensagem de que não existe
+		//4-grava a lista atualizada sem o item removido no arquivo json
+	}
+	
+	public void gravarAnotacoes(ArrayList<Anotacao> anotacoes) throws IOException {
+		FileWriter fileWriter = new FileWriter(caminhoArquivoJson);
+		gson.toJson(anotacoes, fileWriter);
+		fileWriter.flush();
+		fileWriter.close();
+	}
+	
 	public ArrayList<Anotacao> getTodasAnotacoes() throws FileNotFoundException{
 		BufferedReader bufferedReader = new BufferedReader(
-				new FileReader("/home/bruno/eclipse-2019‑06-workspace/anotacoes/dados/anotacoes.json"));
+				new FileReader(caminhoArquivoJson));
 		Type type = new TypeToken<ArrayList<Anotacao>>() {}.getType();
 		ArrayList<Anotacao> anotacoes = gson.fromJson(bufferedReader, type);
 		return anotacoes;
--- src/br/com/anderson/main/AnotacaoApp.java
+++ src/br/com/anderson/main/AnotacaoApp.java
@@ -1,13 +1,21 @@
 package br.com.anderson.main;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import br.com.anderson.json.AnotacaoHandler;
+import br.com.anderson.model.Anotacao;
 
 public class AnotacaoApp {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		System.out.println(new AnotacaoHandler().getTodasAnotacoes());
+	public static void main(String[] args) throws IOException {
+		AnotacaoHandler anotacaoHandler = new AnotacaoHandler();
+		
+		Anotacao anotacao = new Anotacao("meu título 4", "corpo 3");
+		anotacaoHandler.adicionarAnotacao(anotacao);
+		
+		//testar aqui o método removerAnotacao
+		
+		System.out.println(anotacaoHandler.getTodasAnotacoes());
 	}
 
 }
